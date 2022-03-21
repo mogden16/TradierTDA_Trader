63afa39141087cdbb2c9e12240a09d34b2f2d8de
mattogden <Yankees16!>
2022-03-21 13:53:39 -0400

    sell_to_close gmail issue fixed

---
+++ discord/discord_scanner.py
@@ -0,0 +1,107 @@
+import requests
+import config
+from datetime import datetime, timedelta, timezone
+import json
+
+CHANNELID = config.CHANNELID
+DISCORD_AUTH = config.DISCORD_AUTH
+DISCORD_USER = config.DISCORD_USER
+STRATEGY = config.STRATEGY
+
+
+def try_parsing_date(text):
+
+    for fmt in ('%Y-%m-%dT%H:%M:%S.%f%z', '%Y-%m-%dT%H:%M:%S%z'):
+
+        try:
+
+            return datetime.strptime(text, fmt)
+
+        except ValueError:
+
+            pass
+
+    raise ValueError('no valid date format found')
+
+def discord_messages(start_time):
+
+        discord_alerts = []
+
+        headers = {
+            'authorization': DISCORD_AUTH
+        }
+        r = requests.get(
+            f'https://discord.com/api/v9/channels/{CHANNELID}/messages', headers=headers)
+
+        jsonn = json.loads(r.text)
+        # for i in jsonn:
+        #     print(i['timestamp'])
+        x=0
+        for value in jsonn:
+            hedge = False
+            value['timestamp'] = try_parsing_date(value['timestamp'])
+            if value['timestamp'] >= start_time - timedelta(minutes=60):
+                if value['author']['username'] == DISCORD_USER:
+                    if len(value['embeds']) == 0:
+                        continue
+                    statement = value['embeds'][0]['description'].split(' ')
+                    # print(statement)
+                    # if statement[1] == 'Hedge':
+                    #     continue
+                    if statement[1] == 'Hedge':
+                        hedge = True
+                        statement.remove('Hedge')
+                    if statement[1] == "flow,":
+                        statement.remove("flow,")
+                    if statement[1] == "flow":
+                        statement.remove('flow')
+                    if statement[1] == " ":
+                        statement.remove(" ")
+                    if statement[1] == "":
+                        statement.remove("")
+                    if statement[1] == ",":
+                        statement.remove(",")
+                    symbol = statement[0]
+                    str_month = statement[1]
+                    datetime_month_object = datetime.strptime(str_month, '%b')
+                    exp_month = '%02d' % datetime_month_object.month
+                    float_day = float(statement[2])
+                    if float_day < 10:
+                        exp_day = str(0)+statement[2]
+                    else:
+                        exp_day = statement[2]
+                    if statement[3] == "":
+                        statement.remove("")
+                    min_strike = statement[3].split('-')[0].lstrip('$')
+                    max_strike = statement[3].split('-')[1].lstrip('$')
+                    option_type = statement[4].upper()
+                    if option_type[-1] == "S":
+                        option_type = option_type.rstrip(option_type[-1])
+                    option_type_short = option_type[0]
+                    timestamp = value['timestamp']
+                    trade_symbol = f'{symbol}_{exp_month}{exp_day}22{option_type_short}{min_strike}'
+                    list_pre_symbol = {'symbol': symbol, 'exp_month': exp_month, 'exp_day': exp_day,
+                                       'option_type': option_type, 'min_strike': min_strike, 'max_strike': max_strike}
+
+                    obj = {
+                        "Symbol": symbol,
+                        "Side": "BUY_TO_OPEN",
+                        "Pre_Symbol": trade_symbol,
+                        "Exp_Date": f'2022-{exp_month}-{exp_day}',
+                        "Strike_Price": min_strike,
+                        "Option_Type": option_type,
+                        "Strategy": STRATEGY,
+                        "Asset_Type": "OPTION",
+                        "HedgeAlert": "TRUE" if hedge else "FALSE",
+                        "Entry_Date": timestamp
+                    }
+
+                    discord_alerts.append(obj)
+
+        if len(discord_alerts) != 0:
+
+            return discord_alerts
+
+        else:
+
+            return
---
+++ mongo/mongo_helpers.py
@@ -0,0 +1,103 @@
+from pymongo import MongoClient
+import certifi
+import logging
+
+ca = certifi.where()
+
+# MONGO_URI = config.MONGO_URI
+MONGO_URI = 'mongodb+srv://mogden16:Yankees16!@cluster1.6jzau.mongodb.net/Api_Trader?retryWrites=true&w=majority'
+
+
+class MongoFunctions:
+    def __init__(self):
+
+        self.client = None
+
+    def connect(self, logger):
+
+        try:
+
+            logger.info("CONNECTING TO MONGO...", extra={'log': False})
+
+            if MONGO_URI != None:
+
+                self.client = MongoClient(
+                    MONGO_URI, authSource="admin", tlsCAFile=ca)
+
+                # SIMPLE TEST OF CONNECTION BEFORE CONTINUING
+                self.client.server_info()
+
+                db = self.client["Api_Trader"]
+
+                users = db["users"]
+
+                strategies = db["strategies"]
+
+                open_positions = db["open_positions"]
+
+                closed_positions = db["closed_positions"]
+
+                rejected = db["rejected"]
+
+                canceled = db["canceled"]
+
+                queue = db["queue"]
+
+                forbidden = db["forbidden"]
+
+                logger.info("CONNECTED TO MONGO!\n", extra={'log': False})
+
+                return True
+
+            else:
+
+                raise Exception("MISSING MONGO URI")
+
+        except Exception as e:
+
+            logger.error(f"FAILED TO CONNECT TO MONGO! - {e}")
+
+            return False
+
+
+    def disconnect(self):
+
+        self.client.close()
+
+
+    def get_collection(self, col="open_positions"):
+
+        db = MongoFunctions.client["Api_Trader"]
+
+        collection = db[f"{col}"]
+
+        cursor = list(collection.find({}))
+
+        return cursor
+
+    def set_collection(self, col="analysis"):
+
+        db = self.client["Api_Trader"]
+
+
+
+    def mongo_list(self, collection, mongo):
+
+        mongodb = mongo
+
+        col = mongodb[str(collection)]
+
+        col_list = list(col)
+
+        return col_list
+
+
+if __name__ == "__main__":
+
+    main = MongoFunctions()
+
+    main.connect()
+    col = str(input('whats col? '))
+    main.get_collection(col)
+
+    main.disconnect()
