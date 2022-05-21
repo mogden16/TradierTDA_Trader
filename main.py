1db4cce3af7fdc9e5b75421a9cf08ec05f0fca15
Matt Ogden <mogden16@gmail.com>
2022-05-21 14:06:05 -0400

    bug fixes in childorder sellTA

--- main.py
+++ main.py
@@ -563,7 +563,8 @@ class Main(Tasks, TDWebsocket):
                                     if sell_signal:
                                         if RUN_LIVE_TRADER:
                                             if RUN_TRADIER:
-                                                self.tradier.cancel_order(open_position['Order_ID'])
+                                                for childorder in open_position['childOrderStrategies']:
+                                                    self.tradier.cancel_order(childorder['Order_ID'])
                                                 self.set_trader(open_position, trade_signal="CLOSE", trade_type="MARKET")
                                             else:
                                                 print('no exit criteria for TD intraday technical analysis yet')
